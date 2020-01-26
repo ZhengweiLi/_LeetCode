72ee1bcf5018ee4e1e4e94af6d4e72941271381d
ZhengweiLi1994 <36528754+ZhengweiLi1994@users.noreply.github.com>
2020-01-27 00:02:17 +0800

    Update 22_GenerateParentheses.cpp

---
+++ 22_GenerateParentheses.cpp
@@ -0,0 +1,29 @@
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+*/
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        generateParenthesisDFS(n,n,"",res);
+        return res;
+    }
+    void generateParenthesisDFS(int left,int right,string out,vector<string>& res){
+
+        if(left>right) return;
+        if(left==0&&right==0) res.push_back(out);
+        else{
+            if(left>0) generateParenthesisDFS(left-1,right,out+'(',res);
+            if(right>0) generateParenthesisDFS(left,right-1,out+')',res);
+        }
+    }
+};
