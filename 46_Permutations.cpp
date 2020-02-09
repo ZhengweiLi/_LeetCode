777b6a31a9b88cd3783283b5672e2a7fd08bbbdc
ZhengweiLi1994 <36528754+ZhengweiLi1994@users.noreply.github.com>
2020-02-09 23:36:14 +0800

    Update 46_Permutations.cpp

--- 46_Permutations.cpp
+++ 46_Permutations.cpp
@@ -14,3 +14,26 @@ Output:
   [3,2,1]
 ]
 */
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> out, visited(nums.size());
+        permuteDFS(nums, 0, visited, out, res);
+        return res;
+    }
+    void permuteDFS(vector<int>& nums, int level, vector<int>& visited, vector<int>& out, vector<vector<int>>& res){
+        if(level == nums.size()) {res.push_back(out); return ;}
+        for(int i =0; i < nums.size(); i++){
+
+            if(visited[i] == 1) continue;
+            visited[i] = 1;
+
+            out.push_back(nums[i]);
+            permuteDFS(nums, level + 1, visited, out, res);
+            out.pop_back();
+            visited[i] = 0;
+
+        }
+    }
+};
