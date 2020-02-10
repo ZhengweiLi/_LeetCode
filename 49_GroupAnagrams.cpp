f859f6a74079885fb8ff325949d784426fdc12bf
ZhengweiLi1994 <36528754+ZhengweiLi1994@users.noreply.github.com>
2020-02-10 22:11:36 +0800

    加油！

---
+++ 49_GroupAnagrams.cpp
@@ -0,0 +1,36 @@
+/*
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter.
+*/
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        unordered_map<string, int> m;
+
+        for(string str : strs){
+            string t = str;
+            sort(t.begin(), t.end());
+
+            if(!m.count(t)){
+                m[t] = res.size();
+                res.push_back({});
+            }
+            res[m[t]].push_back(str);
+        }
+        return res;
+    }
+};
