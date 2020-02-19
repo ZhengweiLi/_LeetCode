221ad360f30d02da5d7067933974f6fc12353811
ZhengweiLi1994 <36528754+ZhengweiLi1994@users.noreply.github.com>
2020-02-19 19:48:23 +0800

    Update 1110_DeleteNodesandReturnForest.cpp

--- 1110_DeleteNodesandReturnForest.cpp
+++ 1110_DeleteNodesandReturnForest.cpp
@@ -8,3 +8,29 @@ Return the roots of the trees in the remaining forest.  You may return the resul
 Input: root = [1,2,3,4,5,6,7], to_delete = [3,5]
 Output: [[1,2,null,4],[6],[7]]
 */
+class Solution {
+public:
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        vector<TreeNode*> res;
+        set<int> to_delete_set;//这里我想把to_delete换成set做查找 降低效率 感觉vector换成set好麻烦
+        for(int i = 0; i < to_delete.size(); i++){
+            to_delete_set.insert(to_delete[i]);
+        }
+        root = process(root, to_delete_set, res);
+        if(root) res.push_back(root);
+        return res;
+    }
+private:
+    TreeNode* process(TreeNode* root, set<int>& to_delete_set, vector<TreeNode*>& res){//该函数为什么不能直接用public的变量？？
+        if(!root) return nullptr;
+         root->left, root->right = process(root->left, to_delete_set, res), process(root->right, to_delete_set, res);
+
+        if(to_delete_set.find(root->val)!=to_delete_set.end()){
+            if(root->left) res.push_back(root->left);
+            if(root->right) res.push_back(root->right);
+            return nullptr;
+        }
+        return root;
+
+    }
+};//出现runtime error 编译不通过
