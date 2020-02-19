ce19b910490bd3b62273f9e8932edb8011ac591e
ZhengweiLi1994 <36528754+ZhengweiLi1994@users.noreply.github.com>
2020-02-19 22:39:24 +0800

    Update 54_SpiralMatrix.cpp

--- 54_SpiralMatrix.cpp
+++ 54_SpiralMatrix.cpp
@@ -12,3 +12,28 @@ Input:
 ]
 Output: [1,2,3,6,9,8,7,4,5]
 */
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if(matrix.empty() || matrix[0].empty()) return {};
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> res;
+
+        int c = m > n? (n + 1)/2 : (m + 1)/2;
+        int p = m, q = n;
+        for(int i = 0; i < c; i++, p -= 2, q -= 2 ){
+
+            for(int col = i; col <= i+q-1; col++)
+                res.push_back(matrix[i][col]);
+            for(int row = i+1; row <= i+p-1; row++ )
+                res.push_back(matrix[row][i+q-1]);
+
+            if(p == 1 || q == 1) return res;
+            for(int col = i+q-2; col >= i; col--)
+                res.push_back(matrix[i+p-1][col]);
+            for(int row = i+p-2; row >= i+1; row--)
+                res.push_back(matrix[row][i]);
+        }
+        return res;
+    }
+};
