94c7b78a4ca08e25d2dd2d8d6871abb48ee0504a
ZhengweiLi1994 <36528754+ZhengweiLi1994@users.noreply.github.com>
2020-02-22 18:21:56 +0800

    Update 56_MergeIntervals.cpp

--- 56_MergeIntervals.cpp
+++ 56_MergeIntervals.cpp
@@ -7,3 +7,21 @@ Input: [[1,3],[2,6],[8,10],[15,18]]
 Output: [[1,6],[8,10],[15,18]]
 Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 */
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.empty()) return {};
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res{intervals[0]};
+
+        for(int i = 1; i < intervals.size(); i++){
+            if(res.back()[1] < intervals[i][0]){
+                res.push_back(intervals[i]);
+            }
+            else{
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            }
+        }
+        return res;
+    }
+};
