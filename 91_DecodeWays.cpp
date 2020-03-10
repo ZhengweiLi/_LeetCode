21a53f3e87b546496b4e018844c9c9f18bb2e091
ZhengweiLi1994 <36528754+ZhengweiLi1994@users.noreply.github.com>
2020-03-11 00:25:32 +0800

    Create 91_DecodeWays.cpp

---
+++ 91_DecodeWays.cpp
@@ -0,0 +1,35 @@
+/*
+A message containing letters from A-Z
+is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given a non-empty string containing only digits,
+determine the total number of ways to decode it.
+
+Example 1:
+
+Input: "12"
+Output: 2
+Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+*/
+class Solution {
+public:
+    int numDecodings(string s) {
+        if(s.empty() || s[0] == '0') return 0;
+
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+
+        for(int i = 1; i <= n; i++){
+            dp[i] = (s[i-1] == '0')? 0 : dp[i-1];
+
+            if(i > 1 && (s[i-2] == '1' || s[i-2] == '2' && s[i-1] <= '6'))
+               dp[i] += dp[i-2];
+        }
+        return dp.back();
+    }
+};
