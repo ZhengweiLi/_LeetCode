91cab6686feac1b4820b8ee111191d346c7f4add
ZhengweiLi1994 <36528754+ZhengweiLi1994@users.noreply.github.com>
2020-03-28 15:55:04 +0800

    Update 152_MaximumProductSubarray.cpp

---
+++ 152_MaximumProductSubarray.cpp
@@ -0,0 +1,34 @@
+/*
+Given an integer array nums, find the contiguous subarray within an array
+ (containing at least one number) which has the largest product.
+
+Example 1:
+
+Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+*/
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> f(n, 0);
+        vector<int> g(n, 0);
+
+        f[0] = nums[0];
+        g[0] = nums[0];
+        int res = nums[0];
+
+        for(int i = 1; i < n; i++){
+            f[i] = max(max(f[i - 1] * nums[i], g[i - 1] * nums[i]), nums[i]);
+            g[i] = min(min(f[i - 1] * nums[i], g[i - 1] * nums[i]), nums[i]);
+            res = max(res, f[i]);
+        }
+        return res;
+    }
+};
