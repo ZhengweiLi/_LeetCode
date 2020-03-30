5e03926474b2d41eeae01c92e552be7a0df2587f
ZhengweiLi1994 <36528754+ZhengweiLi1994@users.noreply.github.com>
2020-03-30 23:01:52 +0800

    Update 168_ExcelSheetColumnTitle.cpp

--- 168_ExcelSheetColumnTitle.cpp
+++ 168_ExcelSheetColumnTitle.cpp
@@ -25,3 +25,23 @@ Example 3:
 Input: 701
 Output: "ZY"
 */
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string res = "";
+        while(n){
+
+            if(n % 26 == 0){
+                res += 'Z';
+                n -= 26;
+            }
+            else{
+                res += n % 26 - 1 + 'A';
+                n -= n % 26;
+            }
+            n /= 26;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
