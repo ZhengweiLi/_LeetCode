83a192dea35592607d5ee4c8a610657a5b1cfbf0
ZhengweiLi1994 <36528754+ZhengweiLi1994@users.noreply.github.com>
2020-04-05 02:38:30 +0800

    Update 194_TransposeFile.sql

--- 194_TransposeFile.sql
+++ 194_TransposeFile.sql
@@ -17,3 +17,16 @@ Output the following:
 name alice ryan
 age 21 30
 */
+# Read from the file file.txt and print its transposed content to stdout.
+awk '{
+    for(i = 1; i <= NF; i++){
+        if(NR == 1) s[i] = $i;
+        else{
+            s[i] = s[i]" "$i;
+        }
+    }
+}END {
+    for(i = 1; s[i] != ""; i++){
+        print s[i];
+    }
+}' file.txt
