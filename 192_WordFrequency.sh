a80f58b1612f60ca7cb99fbfe64c53293e0187f1
ZhengweiLi1994 <36528754+ZhengweiLi1994@users.noreply.github.com>
2020-04-05 02:12:46 +0800

    20200405

--- 192_WordFrequency.sh
+++ 192_WordFrequency.sh
@@ -19,3 +19,5 @@ is 3
 sunny 2
 day 1
 */
+# Read from the file words.txt and output the word frequency list to stdout.
+grep -oE '[a-z]+' words.txt | sort | uniq -c | sort -nr | awk '{print $2" "$1}'
--- 193_ValidPhoneNumbers.sh
+++ 193_ValidPhoneNumbers.sh
@@ -17,3 +17,5 @@ Your script should output the following valid phone numbers:
 987-123-4567
 (123) 456-7890
 */
+# Read from the file file.txt and output all valid phone numbers to stdout.
+awk '/^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$/' file.txt
