e6fe66299b81275caa003413be49a13becbe7db8
ZhengweiLi1994 <36528754+ZhengweiLi1994@users.noreply.github.com>
2020-04-08 00:45:11 +0800

    Update 188_BestTimetoBuyandSellStockIV.cpp

--- 188_BestTimetoBuyandSellStockIV.cpp
+++ 188_BestTimetoBuyandSellStockIV.cpp
@@ -19,3 +19,28 @@ Output: 7
 Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4.
              Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
 */
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        if(k >= n) k = n;
+
+        vector<int> hold(k + 1,INT_MIN);
+        vector<int> release(k + 1, INT_MIN);
+        int res = INT_MIN;
+        release[0] = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j <= k; j++){
+
+                hold[j] = max(hold[j], release[j - 1] - prices[i]);
+                release[j] = max(release[j], hold[j] + prices[i]);
+            }
+        }
+
+        for(int val : release){
+            res = max(res, val);
+        }
+        return res;
+    }
+};
