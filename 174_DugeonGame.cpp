23b8f94c329f447b1aa1146edf4fd8411113b5a2
ZhengweiLi1994 <36528754+ZhengweiLi1994@users.noreply.github.com>
2020-04-07 23:37:53 +0800

    Update 174_DugeonGame.cpp

--- 174_DugeonGame.cpp
+++ 174_DugeonGame.cpp
@@ -20,3 +20,18 @@ so that he is able to rescue the princess.
 For example, given the dungeon below, the initial health of the knight must be
 at least 7 if he follows the optimal path RIGHT-> RIGHT -> DOWN -> DOWN.
 */
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int m = dungeon.size(), n = dungeon[0].size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
+        dp[m][n - 1] = 1, dp[m - 1][n] = 1;
+
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                dp[i][j] = max(1, min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]);
+            }
+        }
+        return dp[0][0];
+    }
+};
